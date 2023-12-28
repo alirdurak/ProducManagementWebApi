b4c3c01997a7899c3c2ee11e8c967f9be64e7ce3
0Taner <tnr404@gmail.com>
2023-12-28 09:51:33 +0300

    new commit

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,9 +3,9 @@
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public bool IsStatus { get; set; }  
-        public List<Product> products { get; set; }
+        public List<Product>? products { get; set; }
 
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -3,11 +3,11 @@
     public class Product
     {
         public int Id { get; set; } 
-        public string Name { get; set; }    
+        public string? Name { get; set; }    
         public double Price { get; set; }   
         public int Stock { get; set; }  
         public int CategoryId { get; set; } 
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public bool IsStatus { get; set; }  
     }
 }
